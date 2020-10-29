206c52c8f8b857492ad8ed147c825c98c755ae0f
João Victor Corsi Ferreira <jvccorsiferrera@hotmail.com>
2020-10-28 21:10:37 -0300

    Update Receive_datas.java

--- src/Project/Receive_datas.java
+++ src/Project/Receive_datas.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
